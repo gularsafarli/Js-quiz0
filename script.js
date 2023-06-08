4d56d85936527183db4a2574c40b0eef5890a2c9
gular safarli <gularrs@code.edu.az>
2023-06-08 01:32:22 -0700

    js-0

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+function Click(){
+    let input=document.getElementById('inp').value;
+    
+    if(inp.value ===undefined || inp.value===''){
+       alert(' Please fill it');
+    }else{
+       
+    }
+}
+Click();
